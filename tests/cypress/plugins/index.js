2c5d76672e1b71ddd8ec172e7c5c00bfcdc12f66
Mandi <haase1020@gmail.com>
2021-01-25 16:04:45 -0500

    add files

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -12,9 +12,6 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
 module.exports = (on, config) => {
   require("@cypress/code-coverage/task")(on, config);
 
